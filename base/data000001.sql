f7ed08941741fa61e4aae67572dec92246dc0125
ARIEL LOPEZ <ariel.lopez@endecorani.bo>
2019-02-05 12:36:36 -0400

    data000001.sql edited online with Bitbucket

--- base/data000001.sql
+++ base/data000001.sql
@@ -13,6 +13,28 @@ select pxp.f_insert_tgui ('Productos', 'Productos', 'SIAPROD', 'si', 1, 'sis_sia
 
 /********************************************F-DAT-JRR-SIAT-0-16/01/2019********************************************/
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 /********************************************I-DAT-JMH-SIAT-0-05/02/2019********************************************/
 select pxp.f_insert_tgui ('CUF', 'CUF', 'CUF', 'si', 5, 'sis_siat/vista/cuf/Cuf.php', 2, '', 'Cuf', 'SIAT');
 select pxp.f_insert_tgui ('CUIS', 'CUIS', 'CUIS', 'si', 6, 'sis_siat/vista/cuis/Cuis.php', 2, '', 'Cuis', 'SIAT');
